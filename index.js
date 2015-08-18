badba389a485d725876fce5256cc712ea50ac053
Ignacio Martín <ignacio.martin@udg.edu>
2015-08-18 13:33:22 +0200

    Transform module into function receiving a nodi context

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 var Promise = require('bluebird')
 var express = require('express')
 var bodyParser = require('body-parser');
+var router = require('./lib/router')
 
-var router = require.main.require('./ngsi/router')
+/**
+ *
+ * @param ctx This is a dependency injection contex produces by nodi (https://www.npmjs.com/package/nodi)
+ */
+module.exports = function (ctx) {
 
-var app = express()
-app.use(bodyParser.json());
-app.use(router)
+  var app = express()
+  app.use(bodyParser.json());
+  app.use(router(ctx))
 
-NGSI = {}
+  var NGSI = {}
 
-NGSI.start = function (port) {
-  return new Promise(function (resolve, reject) {
-    app.listen(port, function (err) {
-      if (!err) {
-        resolve()
-      } else {
-        reject(err)
-      }
+  NGSI.start = function (port) {
+    return new Promise(function (resolve, reject) {
+      var server = app.listen(port, function (err) {
+        if (!err) {
+          resolve(server)
+        } else {
+          reject(err)
+        }
+      })
     })
-  })
+
+  }
+
+  return NGSI
 }
-
-module.exports = NGSI
-
